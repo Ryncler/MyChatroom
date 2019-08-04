d2127c1672790311fb350daf6872bbee8817b6b2
Rourss <1521093584@qq.com>
2019-08-04 17:42:42 +0800

    建立服务器

---
+++ Server/Server/ServerConntrol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Server
+{
+    class ServerConntrol
+    {
+        private Socket serverSocket;
+        public  ServerConntrol()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Start()
+        {
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any,61100));
+            serverSocket.Listen(5);
+            Console.WriteLine("服务器启动成功！");
+        }
+    }
+}
